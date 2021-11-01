3167fa16f8ebeea7d29162527e3248c715f5c202
Đặng Hoài Bão <worldlystorm2111@gmail.com>
2021-11-01 23:02:27 +0700

    style postShort

--- client/src/Components/Post/PostShort.js
+++ client/src/Components/Post/PostShort.js
@@ -5,25 +5,25 @@ const PostShort = (props) => {
         <div className="PostShort">
             <div className="VoteStatus">
                 <div className="up-button" onClick={props.voteUp}/>
-                <p className="upvote-num">{props.post.upVote}</p>
+                <div className="upvote-num">{props.post.upVote}</div>
                 <div className="down-button" onClick={props.voteDown}/>
             </div>
             <div className="ShortContent">
                 <div className="PostedBy">
                     <img src={props.post.who.avatar} width="30px"/>
-                    <p className="group-name">{props.post.who.groupName}</p>
-                    <p className="user-name">{"Posted by " + props.post.who.userName}</p>
+                    <div className="group-name">{props.post.who.groupName}</div>
+                    <div className="user-name">{"Posted by " + props.post.who.userName}</div>
                 </div>
-                <p className="PostTitle">{props.post.title}</p>
+                <div className="PostTitle">{props.post.title}</div>
                 <div className="PostContent">{props.post.content}</div>
                 <div className="ReactionBar">
                     <div className="Comment">
                         <button onClick={props.commentUp}/>
-                        <p className="comment-count">{props.post.commentCount}</p>
+                        <div className="comment-count">{props.post.commentCount}</div>
                     </div>
                     <div className="Share">
                         <button onClick={props.shareUp}/>
-                        <p className="share-count">{props.post.shareCount}</p>
+                        <div className="share-count">{props.post.shareCount}</div>
                     </div>
                 </div>
             </div>
