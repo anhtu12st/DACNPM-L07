133c2d20def87c16f79d5ebddcda7ddfc69997a4
Đặng Hoài Bão <worldlystorm2111@gmail.com>
2021-11-04 15:27:35 +0700

    GroupFollowing

--- client/src/Components/GroupFollowing/index.js
+++ client/src/Components/GroupFollowing/index.js
@@ -1,4 +1,6 @@
-import './GroupFollowing.module.sass'
+import { FontAwesomeIcon as I } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 import SidePanel from "../SidePanel";
 import style from './GroupFollowing.module.sass'
 
@@ -27,19 +29,20 @@ const GroupFollowing = (props) => {
         }
       ]
 
-  const title = "Nhom Theo Doi"
+    const title = "NHÓM THEO DÕI"
 
-  return (
+    return (
         <SidePanel title={title}>
             <div className={style.listGroup}>
                 {(data.length > 0) && data.map((group) =>
                     <div className={style.group} key={group.id}>
+                        <I icon={faUserCircle} className={style.groupAvatar}/>
                         <div className={style.groupName}>{group.name}</div>
                     </div>
                 )}
             </div>
         </SidePanel>
-  )
+    )
 }
 
 export default GroupFollowing;
