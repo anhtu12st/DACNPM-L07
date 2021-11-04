40c4b8370317603536c1cf06308cc4d92532f364
Đặng Hoài Bão <worldlystorm2111@gmail.com>
2021-11-04 14:14:36 +0700

    fix

---
+++ client/src/Components/GroupProfile/index.js
@@ -0,0 +1,5 @@
+
+const GroupProfile = () => {
+    return (<div/>)
+}
+export default GroupProfile;
--- client/src/Screens/Group/index.js
+++ client/src/Screens/Group/index.js
@@ -1,18 +1,11 @@
-import { Footer } from "../../Components";
 import "./style.sass";
 
 const Group = () => {
     return (
         <div className="Group">
             <div className="left-container">
-                <GroupProfile />
-                <PostView />
             </div>
             <div className="right-container">
-                <GroupAnalysis />
-                <GroupRule />
-                <GroupCurrentQuestions />
-                <Footer />
             </div>
         </div>
     )
