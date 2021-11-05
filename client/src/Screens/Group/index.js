90af6b056925aca1ec6aae06f6dfa782d8ed5e55
sshinichi-69 <khanhgg69@gmail.com>
2021-11-05 07:10:02 +0700

    1

---
+++ client/src/Screens/Group/index.js
@@ -0,0 +1,17 @@
+import "./style.sass";
+import Header from "../../Components/Header/index";
+
+const Group = () => {
+    return (
+        <div>
+            <Header/>
+            <div className="Group">
+                <div className="left-container">
+                </div>
+                <div className="right-container">
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Group;
