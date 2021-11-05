26a10459b6364d5aebfcdbd52dcd579423098a49
Đặng Hoài Bão <worldlystorm2111@gmail.com>
2021-11-05 21:55:53 +0700

    new

--- client/src/App.js
+++ client/src/App.js
@@ -29,11 +29,11 @@ function App() {
             <SignupScreen SignedUp={LoggedIn}/>
           </Route>
           <Route exact path="/profile/:id" component={ProfileScreen} />
-          <Route exact path="/" component={GroupScreen} />
+          <Route exact path="/group" component={GroupScreen} />
           <Route exact path="/createpost" component={CreatePostScreen} />
           {/* <Route exact path="/post:id" component={PostDetailScreen} /> */}
           <Route>
-            {/*<Redirect to="/" />*/}
+            <Redirect to="/" />
           </Route>
         </Layout>
       </Switch>
--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {faChevronDown, faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faChevronDown, faSearch, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
 import {faUser} from '@fortawesome/free-regular-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Link} from "react-router-dom";
 import style from './Header.module.sass';
 
-const Header = () => {
+const Header = (props) => {
   return (
       <div className={style.headerBar}>
         <Link to='/'>
@@ -16,14 +16,18 @@ const Header = () => {
           <input placeholder='Search CSE Connector'/>
           {/*<button type='submit'><FontAwesomeIcon icon={faSearch}/></button>*/}
         </div>
-        <div className={style.userLogin}>
-          <button className={style.btn}>Login</button>
-          <button className={style.btn}> Sign Up</button>
-          <div className={style.userDropdownList}>
-            <FontAwesomeIcon icon={faUser}/>
-            <FontAwesomeIcon icon={faChevronDown}/>
-          </div>
-        </div>
+        <div className={style.userLogin} style={{ "justify-content": props.isLoggedIn ? "flex-end" : "space-around" }}>
+                <Link style={{ display: props.isLoggedIn ? "none" : "block" }} to='/login' className={style.btn}>Login</Link>
+                <Link style={{ display: props.isLoggedIn ? "none" : "block" }} to='/signup' className={style.btn}>Sign Up</Link>
+                <div className={style.userDropdownList} style={{
+                    display: props.isLoggedIn ? "flex" : "none"
+                }}>
+                    <Link to='/profile/123'>
+                        <FontAwesomeIcon icon={faUser} />
+                    </Link>
+                    <div onClick={props.logOut}><FontAwesomeIcon icon={faSignOutAlt} /></div>
+                </div>
+            </div>
       </div>
   );
 }
