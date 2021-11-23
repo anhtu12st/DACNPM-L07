d47dc3e4527bdeab2c1aaeec671024dbe6f07228
Dangnguyen2705 <ndang9a1@gmail.com>
2021-11-23 19:30:21 +0700

    export listPosts

--- client/src/Services/Post.js
+++ client/src/Services/Post.js
@@ -4,3 +4,9 @@ export const createPost = async (form) => {
     const url = '/post';
     await Axios.post(url, form);
 }
+
+export const listPosts = async () => {
+    const url = '/post';
+    const { data } = await Axios.get(url);
+    return data;
+}
